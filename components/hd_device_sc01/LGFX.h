8c1e79ba4033886d2f574082ee588d7f094d4f20
RSV <srepenko@gmail.com>
2025-04-01 18:46:32 +0300

    1

--- components/hd_device_sc01/LGFX.h
+++ components/hd_device_sc01/LGFX.h
@@ -12,7 +12,7 @@ namespace hd_device {
 class LGFX : public lgfx::LGFX_Device
 {
   lgfx::Panel_ST7796 _panel_instance;
-  lgfx::Bus_Parallel8 _bus_instance;
+  lgfx::Bus_SPI _bus_instance;
   lgfx::Light_PWM _light_instance;
   lgfx::Touch_FT5x06 _touch_instance;
 
