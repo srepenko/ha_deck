fca29e17a3fb1389bdecbf4f1bb2d8b8cf8553e1
RSV <srepenko@gmail.com>
2025-04-02 14:18:11 +0300

    1

--- components/hd_device_sc01/hd_device_sc01.cpp
+++ components/hd_device_sc01/hd_device_sc01.cpp
@@ -14,20 +14,23 @@ lv_group_t *group;
 
 void IRAM_ATTR flush_pixels(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
 {
+/*
     uint32_t w = (area->x2 - area->x1 + 1);
     uint32_t h = (area->y2 - area->y1 + 1);
     uint32_t len = w * h;
 
-    lcd.startWrite();                            /* Start new TFT transaction */
-    lcd.setAddrWindow(area->x1, area->y1, w, h); /* set the working window */
+    lcd.startWrite();                            
+    lcd.setAddrWindow(area->x1, area->y1, w, h); 
     lcd.writePixels((uint16_t *)&color_p->full, len, true);
-    lcd.endWrite();                              /* terminate TFT transaction */
+    lcd.endWrite();                              
 
     lv_disp_flush_ready(disp);
+*/
 }
 
 void IRAM_ATTR touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 {
+    /*
     uint16_t touchX, touchY;
     bool touched = lcd.getTouch(&touchX, &touchY);
 
@@ -38,6 +41,7 @@ void IRAM_ATTR touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data
     } else {
         data->state = LV_INDEV_STATE_REL;
     }
+    */
 }
 
 void HaDeckDevice::setup() {
