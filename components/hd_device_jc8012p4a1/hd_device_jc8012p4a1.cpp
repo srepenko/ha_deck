57c5bbc0f24f343c9ed91c200165efe1eaabfd0b
RSV <srepenko@gmail.com>
2025-12-03 10:02:08 +0000

    1

--- components/hd_device_jc8012p4a1/hd_device_jc8012p4a1.cpp
+++ components/hd_device_jc8012p4a1/hd_device_jc8012p4a1.cpp
@@ -3,7 +3,21 @@
 namespace esphome {
 namespace hd_device {
 
+jd9365_lcd lcd = jd9365_lcd(LCD_RST);
+gsl3680_touch touch = gsl3680_touch(TP_I2C_SDA, TP_I2C_SCL, TP_RST, TP_INT);
 
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t *buf;
+static lv_color_t *buf1
+
+void HaDeckDevice::setup() {
+    lv_init();
+    lv_theme_default_init(NULL, lv_color_hex(0xFFEB3B), lv_color_hex(0xFF7043), 1, LV_FONT_DEFAULT);
+
+    lcd.init();
+
+    lv_disp_draw_buf_init(&draw_buf, buf, NULL, TFT_HEIGHT * 20);
+}
 
 }  // namespace hd_device
 }  // namespace esphome
